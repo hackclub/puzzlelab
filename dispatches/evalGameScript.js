f038bd55592ef52356d5f86ba0882c08a91ed2c6
Cedric Hutchings <ced@misguided.enterprises>
2022-07-06 13:54:22 -0400

    automatically clear set timeouts

--- dispatches/evalGameScript.js
+++ dispatches/evalGameScript.js
@@ -3,13 +3,19 @@ import { init } from "../engine/engine.js";
 import { playTune } from "../engine/playTune.js";
 
 let tunes = [];
+let ticks = [];
 
 export function evalGameScript(script, palette) {
   const canvas = document.querySelector(".game-canvas");
   const gameFunctions = init(canvas);
-  
+
   tunes.forEach(t => t.end());
   tunes = [];
+
+  ticks.forEach(clearInterval);
+  ticks = [];
+
+  gameFunctions.setInterval = (fn, n) => ticks.push(setInterval(fn, n));
 
   gameFunctions.playTune = (tune, n) => {
     const x = playTune(tune, n);
