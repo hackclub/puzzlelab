35d92cb1129828c324c68b320abfbd2595792554
Belle <bellesee1212@gmail.com>
2022-07-07 21:34:52 +0800

    gallery

---
+++ gallery/src/routes/index.svelte
@@ -0,0 +1,393 @@
+<script>
+	import { onMount } from 'svelte';
+	import VanillaTilt from 'vanilla-tilt';
+
+	onMount(()=> {
+		VanillaTilt.init(document.querySelectorAll(".gallery-item"), {
+			max: 15,
+			scale: 1.05
+		});
+	})
+	import { apiData, Data, imgData, Img } from '../data';
+
+	onMount(async () => {
+		fetch('https://api.github.com/repos/hackclub/puzzlelab/contents/games?recursive=1')
+			.then((res) => res.json())
+			.then((data) => {
+				console.log(data);
+				apiData.set(data);
+			})
+			.catch((error) => {
+				console.log(error);
+				return [];
+			});
+
+		fetch('https://api.github.com/repos/hackclub/puzzlelab/contents/dist/assets')
+			.then((res) => res.json())
+			.then((data) => {
+				imgData.set(data);
+			});
+
+		var iframe = document.getElementsByTagName('iframe');
+		for (let i = 0; i < iframe.length; i++) {
+			var elmnt = iframe.contentWindow.document.getElementsByClassName('run')[0];
+		}
+		console.log('element:' + elmnt);
+	});
+</script>
+
+<body>
+	<div class="info-outer">
+		<div class="info-inner">
+			<div class="title">
+				<h1>PuzzleLab Gallery</h1>
+				<p>
+					The best way to learn is by making things you care about and sharing them with others.
+					Check out games by other Hack Clubbers!
+				</p>
+			</div>
+			<div>
+				<a href="https://puzzle.hackclub.com" class="button big-button">Create Your Own</a>
+				<a href="/share" class="button big-button">Share your game</a>
+			</div>
+		</div>
+	</div>
+		<a class="logo" href="https://hackclub.com"
+		><img src="https://assets.hackclub.com/flag-orpheus-top.svg" alt="hack club logo" /></a
+	>
+	<a href="https://github.com/hackclub/puzzlelab" target="_blank"
+		><img src="./github.svg" alt="github logo" class="github-logo" /></a
+	>
+	<div class="gallery-outer">
+		<div class="gallery-inner">
+			{#each $Data as data}
+				{#if data.name == '.DS_Store'}
+					<div class="gallery-item">
+						<a href={`https://puzzlelab.hackclub.dev/`}>
+							<div class="image-box">
+								<iframe
+									src="https://puzzlelab.hackclub.dev/?file=https://hackclub.github.io/puzzlelab/games/maze.js"
+									title={data.name}
+								/>
+							</div>
+							<div class="text">
+								<h3>Your Game</h3>
+								<a href={`https://puzzlelab.hackclub.dev/`} target="_blank" class="button"
+									>Create &#9658;</a
+								>
+							</div>
+						</a>
+					</div>
+				{:else}
+					<div class="gallery-item">
+						<a href={`https://puzzlelab.hackclub.dev/?file=${data.html_url}`}>
+							<div class="image-box">
+								<img
+									src="https://raw.githubusercontent.com/bellesea/gamelab-gallery-test/master/{data.name.replace(
+										'.js',
+										''
+									)}.png"
+									class="image"
+									alt="game preview"
+								/>
+							</div>
+							<div class="text">
+								<h3>
+									{data.name
+										.replace('.js', '')
+										.replace('_', ' ')
+										.replace('-', ' ')
+										.replace('.', '')}
+								</h3>
+								<a
+									href={`https://puzzlelab.hackclub.dev/?file=${data.html_url}`}
+									target="_blank"
+									class="button">Play &#9658;</a
+								>
+							</div>
+						</a>
+					</div>
+				{/if}
+			{/each}
+		</div>
+	</div>
+</body>
+
+<style>
+	@import url('https://fonts.googleapis.com/css2?family=B612+Mono:ital,wght@0,400;0,700;1,400;1,700&display=swap');
+
+	:root {
+		--pcb-lighter: #04a356;
+		--pcb-base: #016e3d;
+		--pcb-trace: #014a27;
+		--pcb-darker: #03321b;
+	}
+
+	body {
+		font-family: 'B612 Mono', monospace;
+		font-weight: 400;
+		color: white;
+		margin: auto;
+	}
+
+	h1::selection,
+	h3::selection,
+	p::selection,
+	a::selection {
+		color: var(--pcb-trace);
+		background-color: white;
+	}
+
+	h1 {
+		font-size: 24px;
+		margin: 0;
+	}
+
+	h3 {
+		margin: 0;
+		text-transform: capitalize;
+		color: white;
+		margin-top: 10px;
+		font-size: 15px;
+		transform: translateZ(20px);
+	}
+
+	a {
+		text-decoration: none;
+	}
+
+	p {
+		font-size: 13px;
+	}
+
+	.info-outer {
+		background-color: var(--pcb-base);
+		background-image: url(https://puzzlelab.hackclub.dev/assets/bg.12a2b49c.svg);
+		padding: 10vh 0;
+		width: 100vw;
+	}
+
+	.info-inner {
+		width: 85%;
+		margin: auto;
+	}
+
+	.title {
+		margin-bottom: 50px;
+	}
+	.gallery-outer {
+		width: 100vw;
+		background-color: #014a27;
+		height: fit-content;
+		min-height: 100vh;
+		padding: 40px 0;
+	}
+	.gallery-inner {
+		width: 85vw;
+		margin: auto;
+		display: grid;
+		column-gap: 30px;
+		row-gap: 30px;
+	}
+
+	.gallery-item {
+		background-color: var(--pcb-base);
+		padding: 15px 13px;
+		border-radius: 5px;
+		border-bottom: var(--pcb-darker) 10px solid;
+		transition-duration: 0.5s;
+		transform-style: preserve-3d;
+		transform: perspective(1000px);
+	}
+
+	/* .gallery-item:hover {
+		transform: scale(1.08) rotate(-1.5deg);
+		cursor: pointer;
+	} */
+
+	.button {
+		background-color: var(--pcb-trace);
+		padding: 5px 10px;
+		font-size: 0.8em;
+		color: var(--pcb-lighter);
+		text-decoration: none;
+		font-weight: 700;
+		border-radius: 3px;
+		margin-left: auto;
+		transition-duration: 0.5s;
+		margin-top: 10px;
+		transform: translateZ(20px);
+	}
+
+	.button:hover {
+		transform: scale(1.12) rotate(3deg);
+	}
+
+	.big-button {
+		position: relative;
+		font-size: 16px;
+		background-color: transparent;
+		color: white;
+		border: white 2px dotted;
+	}
+
+	.big-button:hover {
+		background-color: white;
+		color: var(--pcb-base);
+		border: white 2px solid;
+	}
+
+	.text {
+		padding: 10px 0;
+		display: flex;
+		flex: 60% 40%;
+		flex-wrap: wrap;
+	}
+
+	.github-logo {
+		color: white;
+		position: absolute;
+		right: 30px;
+		top: 30px;
+		fill: white;
+		width: 24px;
+		transition-duration: 0.5s;
+	}
+
+	.github-logo:hover {
+		transform: rotate(9deg);
+	}
+
+	.image-box {
+		width: 100%;
+		height: 250px;
+		overflow: hidden;
+		transform: translateZ(20px);
+	}
+
+	.image {
+		width: 100%;
+		height: 100%;
+		border-radius: 3px;
+		object-fit: cover;
+		transform: translateZ(20px);
+	}
+
+	.logo {
+		position: absolute;
+		top: 0;
+		left: 30px;
+		background-size: contain;
+		cursor: pointer;
+		flex-shrink: 0;
+		z-index: 333;
+		transition: cubic-bezier(0.375, 0, 0.675, 1) transform;
+		transform-origin: top left;
+	}
+
+	.logo img {
+		width: 112px;
+		height: auto;
+	}
+
+	.logo:hover,
+	.logo:focus {
+		animation: waveFlag 0.5s linear infinite alternate;
+	}
+
+	@keyframes waveFlag {
+		from {
+			transform: rotate(0deg);
+		}
+		to {
+			transform: rotate(-5deg);
+		}
+	}
+
+	@media (min-width: 320px) {
+		.gallery-inner {
+			grid-template-columns: 1fr;
+		}
+
+		.github-logo {
+			width: 24px;
+		}
+	}
+
+	@media (min-width: 480px) {
+		h1 {
+			font-size: 26px;
+		}
+
+		h3 {
+			font-size: 18px;
+		}
+
+		p {
+			font-size: 14px;
+		}
+
+		.gallery-inner {
+			grid-template-columns: 1fr 1fr;
+		}
+
+		.github-logo {
+			width: 36px;
+		}
+
+		.big-button {
+			position: relative;
+			font-size: 20px;
+		}
+
+		.image-box {
+			height: 200px;
+		}
+	}
+
+	@media (min-width: 760px) {
+		h1 {
+			font-size: 42px;
+		}
+
+		h3 {
+			font-size: 20px;
+		}
+
+		.gallery-inner {
+			grid-template-columns: 1fr 1fr 1fr;
+		}
+
+		.github-logo {
+			width: 38px;
+		}
+
+		.big-button {
+			position: relative;
+			font-size: 20px;
+		}
+
+		.image-box {
+			height: 200px;
+		}
+	}
+
+	@media (min-width: 920px) {
+		h1 {
+			font-size: 54px;
+		}
+
+		p {
+			font-size: 18px;
+		}
+
+		.gallery-item {
+			padding: 30px 25px;
+		}
+
+		.logo img {
+			width: 128px;
+			height: auto;
+		}
+	}
+</style>
---
+++ gallery/src/routes/share.svelte
@@ -0,0 +1,265 @@
+<script>
+	import { onMount } from 'svelte';
+
+	onMount(() => {
+		fetch('https://puzzlelab.hackclub.dev/?file=https://hackclub.github.io/puzzlelab/games/maze.js')
+			.then((resp) => resp.text())
+			.then((d) => {
+				console.log('d:' + d);
+			});
+	});
+
+	import VanillaTilt from 'vanilla-tilt';
+
+	onMount(() => {
+		VanillaTilt.init(document.querySelectorAll('.media'), {
+			max: 15,
+			scale: 1.05
+		});
+	});
+</script>
+
+<body>
+	<a class="logo" href="https://hackclub.com"
+		><img src="https://assets.hackclub.com/flag-orpheus-top.svg" alt="hack club logo" /></a
+	>
+	<a href="https://github.com/hackclub/puzzlelab" target="_blank"
+		><img src="./github.svg" alt="github logo" class="github-logo" /></a
+	>
+	<div class="outer">
+		<div class="heading-outer">
+			<div class="heading-inner">
+				<h1>Share your game</h1>
+				<p>
+					After you <a href="https://puzzlelab.hackclub.dev/" target="_blank" class="link"
+						>create your own game</a
+					>, we'd love for you to <a href="/" class="link">share it with other makers</a> on the internet!
+				</p>
+			</div>
+		</div>
+		<div class="inner">
+			<div class="step">
+				<div>
+					<p class="step-number">Step 1:</p>
+					<h2>Name your game</h2>
+					<p>
+						Add the following comment. For your title, you want to make sure there are no spaces as
+						this will translate to your file name.
+					</p>
+				</div>
+				<code class="media"
+					>/*
+					<br />
+					@title: your_game <br />
+					@author: your name
+					<br />
+					*/
+				</code>
+			</div>
+			<div class="step">
+				<div>
+					<p class="step-number">Step 2:</p>
+					<h2>Download your game file</h2>
+					<p>Select export then js. Your download should begin immediately.</p>
+				</div>
+				<img
+					class="media"
+					src="https://cloud-hemofvnr5-hack-club-bot.vercel.app/0download.png"
+					alt="screenshot of where to export game file"
+				/>
+			</div>
+			<div class="step">
+				<div>
+					<p class="step-number">Step 3:</p>
+					<h2>
+						Upload your game to the <a
+							href="https://github.com/hackclub/puzzlelab/tree/main/games"
+							target="_blank"
+							class="link">PuzzleLab Repository</a
+						>
+					</h2>
+					<ul>
+						<li>Open the "Games" folder within the PuzzleLab Repository</li>
+						<li>Click "Add File"</li>
+						<li>Select "Upload File"</li>
+					</ul>
+				</div>
+				<img
+					class="media"
+					src="https://cloud-3y74f3v9a-hack-club-bot.vercel.app/0screenshot_2022-07-07_at_4.49.28_pm.png"
+					alt="screenshot of where to upload game file option is"
+				/>
+			</div>
+			<div class="step">
+				<div>
+					<p class="step-number">Step 4:</p>
+					<h2>Create Pull Request</h2>
+					<ul>
+						<li>Scroll down and click propose new file</li>
+						<li>Select "Create Pull Request"</li>
+						<li>Click "Create Pull Request"</li>
+					</ul>
+					<p>Once we approve it! Your game will appear in the gallery :)</p>
+				</div>
+				<img
+					class="media"
+					src="https://cloud-n8au370qf-hack-club-bot.vercel.app/0screenshot_2022-07-07_at_8.59.28_pm.png"
+					alt="screenshot of pull request page"
+				/>
+			</div>
+		</div>
+	</div>
+</body>
+
+<style>
+	:root {
+		--pcb-lighter: #04a356;
+		--pcb-base: #016e3d;
+		--pcb-trace: #014a27;
+		--pcb-darker: #03321b;
+	}
+
+	body {
+		font-family: 'B612 Mono', monospace;
+		font-weight: 400;
+		color: white;
+		min-height: 100vh;
+		width: 100vw;
+		background-color: #014a27;
+		height: fit-content;
+		padding: 40px 0;
+		color: white;
+		padding: 0;
+		margin: 0;
+	}
+
+	code {
+		display: block;
+		background-color: var(--pcb-darker);
+		padding: 20px 30px;
+		border-radius: 5px;
+		width: 65%;
+		margin: auto;
+	}
+
+	img {
+		width: 100%;
+		margin: auto;
+		height: auto;
+	}
+
+	li {
+		line-height: 1.5em;
+	}
+
+	h2 {
+		margin: 0 0 20px 0;
+	}
+
+	.link {
+		color: white;
+		background-color: var(--pcb-darker);
+		padding: 2px 4px;
+		border-radius: 2px;
+		text-decoration: none;
+		white-space: nowrap;
+	}
+
+	.inner {
+		width: 85vw;
+		margin: auto;
+	}
+
+	.heading-outer {
+		padding: 10vh 0;
+		width: 100vw;
+		margin: 0;
+		margin-bottom: 5vh;
+		background: var(--pcb-base);
+		background-image: url(https://puzzlelab.hackclub.dev/assets/bg.12a2b49c.svg);
+	}
+
+	.heading-inner {
+		width: 85%;
+		margin: auto;
+	}
+
+	.step {
+		display: grid;
+		grid-template-columns: 1fr;
+		padding-bottom: 7vh;
+	}
+
+	.step-number {
+		margin: 0;
+	}
+
+	.github-logo {
+		color: white;
+		position: absolute;
+		right: 30px;
+		top: 30px;
+		fill: white;
+		width: 24px;
+		transition-duration: 0.5s;
+	}
+
+	.github-logo:hover {
+		transform: rotate(9deg);
+	}
+
+	.logo {
+		/* background-image: url(https://assets.hackclub.com/flag-orpheus-top.svg); */
+		/* background-repeat: no-repeat; */
+		/* background-position: top left; */
+		position: absolute;
+		top: 0;
+		left: 30px;
+		background-size: contain;
+		cursor: pointer;
+		flex-shrink: 0;
+		z-index: 333;
+		transition: cubic-bezier(0.375, 0, 0.675, 1) transform;
+		/* transition: ${3 / 16}s cubic-bezier(0.375, 0, 0.675, 1) transform; */
+		transform-origin: top left;
+	}
+
+	.logo img {
+		width: 112px;
+		height: auto;
+	}
+
+	.logo:hover,
+	.logo:focus {
+		animation: waveFlag 0.5s linear infinite alternate;
+	}
+
+	@keyframes waveFlag {
+		from {
+			transform: rotate(0deg);
+		}
+		to {
+			transform: rotate(-5deg);
+		}
+	}
+
+	@media (min-width: 760px) {
+		h1 {
+			font-size: 42px;
+		}
+
+		.step {
+			grid-template-columns: 0.8fr 1.2fr;
+		}
+
+		@media (min-width: 920px) {
+			h1 {
+				font-size: 54px;
+			}
+		}
+
+		img {
+			width: 80%;
+		}
+	}
+</style>
