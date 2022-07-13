36a3b7ed39d627ae58e2b63fcc207530b53ffaf3
leomcelroy <leomcelroy@gmail.com>
2022-07-13 16:15:25 -0400

    Update engine.js

--- engine/engine.js
+++ engine/engine.js
@@ -202,7 +202,7 @@ export function init({ palette, setBitmaps, setScreenSize }) {
     const grid = getGrid();
     for (let x = 0; x < width; x++) {
       for (let y = 0; y < height; y++) {
-        const tile = state.grid[width*y+x] || [];
+        const tile = grid[width*y+x] || [];
         const matchIndices = matchingTypes.map(type => {
           return tile.map(s => s.type).indexOf(type);
         })
